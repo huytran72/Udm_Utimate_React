29201bd0b57284841436f5764718b1b90543ccca
Huy Tran <baohuy82.th@gmail.com>
2025-01-08 15:05:21 -0800

    Update script.js

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -312,3 +312,9 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((data) => console.log(data));
 console.log("kenny");
+
+async function getTodos() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  console.log(data);
+}
