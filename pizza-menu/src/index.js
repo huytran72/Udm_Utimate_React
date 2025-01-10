263d16b5690d77c57166360b694ab8e4e218aaf8
Huy Tran <baohuy82.th@gmail.com>
2025-01-10 13:08:12 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -58,6 +58,7 @@ function App() {
 function Pizza() {
   return (
     <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
