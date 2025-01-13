f06015ce47211288d5f905173e5ae58033637070
Huy Tran <baohuy82.th@gmail.com>
2025-01-12 23:35:31 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -57,7 +57,9 @@ function App() {
 }
 
 function Header() {
-  return <h1 style={{ color: "red" }}>Fast React Pizza Co.</h1>;
+  return (
+    <h1 style={{ color: "red", fontSize: "32px" }}>Fast React Pizza Co.</h1>
+  );
 }
 
 function Menu() {
