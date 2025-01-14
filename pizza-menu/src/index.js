c8c08c91cedf8d8760fedf0c57a7860b7fc45326
Huy Tran <baohuy82.th@gmail.com>
2025-01-13 23:39:55 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -97,8 +97,12 @@ function Pizza(props) {
   return (
     <div className="pizza">
       <img src={props.photoName} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+        <button>Add to cart </button>
+      </div>
     </div>
   );
 }
