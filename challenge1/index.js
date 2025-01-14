d77ba0f80e15cac5b35c85a7301beb16d5e44956
Huy Tran <baohuy82.th@gmail.com>
2025-01-14 14:31:15 -0800

    Update index.js

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,3 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 import App from "./App";
+
+const rootElement = createRoot(document.getElementById("root"));
+const root = createRoot(rootElement);
