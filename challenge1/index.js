37b51618545cb62aab10a465ce0205ac3986b66f
Huy Tran <baohuy82.th@gmail.com>
2025-01-15 23:29:43 -0800

    Update index.js

--- challenge1/index.js
+++ challenge1/index.js
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 import App from "./App";
 
-const rootElement = createRoot(document.getElementById("root"));
-const root = createRoot(rootElement);
-
 function App() {
   return (
     <div className="card">
@@ -12,6 +9,9 @@ function App() {
     </div>
   );
 }
+
+const rootElement = createRoot(document.getElementById("root"));
+const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
