504274b8817ab83bfe3304dc16c9508d38d3f4a6
Huy Tran <baohuy82.th@gmail.com>
2025-01-17 14:29:04 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -75,7 +75,7 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul>
+      <ul className="pizzas">
         {pizzaData.map((pizza) => (
           <Pizza pizzaObj={pizza} key={pizza.name} />
         ))}
