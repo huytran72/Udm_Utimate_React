5c6e11d26c171cc19a5c582f8f63e1894d7e926c
Huy Tran <baohuy82.th@gmail.com>
2025-01-17 14:31:42 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -79,6 +79,11 @@ function Menu() {
         {pizzaData.map((pizza) => (
            <li className="pizza">
             <img src={pizza.photoName} alt={pizza.name} />
+            <div>
+              <h3>{pizza.name}</h3>
+              <p>{pizza.ingredients}</p>
+              <span>{pizza.price + 3}</span>
+              <button>Add to cart </button>
         ))}
       </ul>
 
