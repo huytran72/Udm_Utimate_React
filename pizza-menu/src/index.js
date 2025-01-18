708e74836cc03a5569153932f51987d94d71dff5
Huy Tran <baohuy82.th@gmail.com>
2025-01-18 10:00:25 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -78,12 +78,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {pizzas && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
       )}
 
       {/* <Pizza
