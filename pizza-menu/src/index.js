d25fcda7755aa988ad32551f426f8adb20d1be0a
Huy Tran <baohuy82.th@gmail.com>
2025-01-19 23:37:34 -0800

    Update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -79,16 +79,19 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>
-        Authentic Italian cuisine made with the freshest ingredients. Order now
-      </p>
-
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <div>
+          <p>
+            Authentic Italian cuisine made with the freshest ingredients. Order
+            now
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </div>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}
