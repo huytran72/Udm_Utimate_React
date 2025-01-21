b84091586dd0a6367354c3c7c0fb251c3d010235
Huy Tran <baohuy82.th@gmail.com>
2025-01-20 22:00:04 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="steps">
-      <div className="number">
+      <div className="numbers">
         <div>1</div>
         <div>2</div>
         <div>3</div>
