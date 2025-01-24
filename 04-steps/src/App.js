053d8ff11133f0a86e41d2f4caa33d319c2bf062
Huy Tran <baohuy82.th@gmail.com>
2025-01-23 23:40:23 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -11,7 +11,9 @@ export default function App() {
     alert("Previous");
   };
 
-  const handleNext = () => {};
+  const handleNext = () => {
+    alert("Next");
+  };
 
   return (
     <div className="steps">
