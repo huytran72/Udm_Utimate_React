c98005bbf016c43ea6d475146f349f3724f430fe
Huy Tran <baohuy82.th@gmail.com>
2025-01-23 23:51:31 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -22,7 +22,7 @@ export default function App() {
       <div className="numbers">
         <div className={step >= 1 ? "active" : ""}>1</div>
         <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
