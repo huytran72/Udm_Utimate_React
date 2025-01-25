4a1fdd104bc72b2a01d7c575179aec92345cb8cb
Huy Tran <baohuy82.th@gmail.com>
2025-01-24 23:44:40 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -22,32 +22,36 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      {
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-        {test.name}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {test.name}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => alert("Previous step")}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => alert("Next step")}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("Previous step")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("Next step")}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      }
     </div>
   );
 }
