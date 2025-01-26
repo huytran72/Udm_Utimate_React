2c7e85134d9489756ef50c6c7fa7681e5bd93a2a
Huy Tran <baohuy82.th@gmail.com>
2025-01-25 23:50:55 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -16,58 +16,57 @@ export default function App() {
 }
 
 function Steps() {
-    const [step, setStep] = useState(1);
-    const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
 
-    // const [test, setTest] = useState({ name: "Kenny Tran" });
+  // const [test, setTest] = useState({ name: "Kenny Tran" });
 
-    function handlePrevious() {
-      if (step > 1) setStep((s) => s - 1);
-    }
-
-    function handleNext() {
-      if (step < 3) {
-        setStep((s) => s + 1);
-        // setStep((s) => s + 1);
-      }
-    }
-
-    return (
-      <>
-        <button className="close" onClick={() => setIsOpen(!isOpen)}>
-          &time;
-        </button>
-
-        {isOpen && (
-          <div className="steps">
-            <div className="numbers">
-              <div className={step >= 1 ? "active" : ""}>1</div>
-              <div className={step >= 2 ? "active" : ""}>2</div>
-              <div className={step >= 3 ? "active" : ""}>3</div>
-            </div>
-
-            <p className="message">
-              Step {step}: {messages[step - 1]}
-              {/* {test.name} */}
-            </p>
-
-            <div className="buttons">
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                onClick={() => alert("Previous step")}
-              >
-                Previous
-              </button>
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                onClick={() => alert("Next step")}
-              >
-                Next
-              </button>
-            </div>
-          </div>
-        )}
-      </>
-    );
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
   }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &time;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("Previous step")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("Next step")}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
