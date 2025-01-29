844e7668c92523300c6f4a7783094a40dc653779
Huy Tran <baohuy82.th@gmail.com>
2025-01-28 23:13:00 -0800

    Update App.js

--- assign1/App.js
+++ assign1/App.js
@@ -25,7 +25,13 @@ function Counter() {
       </div>
 
       <p>
-        <span>{count === 0 ? "Today is" : ""}</span>
+        <span>
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from today is`
+            : `{count} days ago was`}
+        </span>
         <span>{date.toDateString()} </span>
       </p>
     </div>
