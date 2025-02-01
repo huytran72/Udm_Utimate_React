de13a8a569252717e68325fa3c727426d9d3930a
Huy Tran <baohuy82.th@gmail.com>
2025-01-31 22:50:34 -0800

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -26,6 +26,7 @@ function Form() {
       <select>
         <option value={1}>1</option>
         <option value={2}>2</option>
+        <option value={2}>2</option>
       </select>
     </form>
   );
