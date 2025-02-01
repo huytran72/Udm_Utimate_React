97a7c4cf3a87ad2620d19676b171be565c71e6a0
Huy Tran <baohuy82.th@gmail.com>
2025-01-31 22:59:11 -0800

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -25,7 +25,7 @@ function Form() {
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
       <h3>What do you need for your trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
