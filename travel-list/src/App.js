315ef23ea17d5ec7a4b7561a0fd768758329d1aa
Huy Tran <baohuy82.th@gmail.com>
2025-01-31 22:53:28 -0800

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -23,12 +23,7 @@ function Form() {
   return (
     <form className="add-form">
       <h3>What do you need for your trip?</h3>
-      <select>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
-        <option value={4}>4</option>
-      </select>
+      <select>{Array.from({ length: 20 }, (_, i) => i + 1)}</select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
     </form>
