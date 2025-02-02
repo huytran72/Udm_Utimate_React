82ca9249d9418566f7c16c47860684d10ae3a7f3
Huy Tran <baohuy82.th@gmail.com>
2025-02-01 23:39:20 -0800

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -23,7 +23,7 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("TEST");
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(5);
 
   function handleSubmit(e) {
     e.preventDefault();
