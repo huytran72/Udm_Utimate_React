c0e72f60a10c373c695c10c6da998919b02d252d
Huy Tran <baohuy82.th@gmail.com>
2025-02-04 14:38:45 -0800

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <div className="app">
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
