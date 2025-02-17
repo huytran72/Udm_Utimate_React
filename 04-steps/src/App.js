1c77968df4a4f602bce20be6378bb581f19678ff
Huy Tran <baohuy82.th@gmail.com>
2025-02-16 23:27:43 -0800

    Update App.js

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -70,7 +70,7 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text, emoji, side }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
