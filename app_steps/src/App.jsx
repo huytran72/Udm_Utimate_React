469c0b5e1cdfe5f9462846aeac611051ab492328
Huy Tran <baohuy82.th@gmail.com>
2025-02-18 23:44:27 -0800

    Update App.jsx

--- app_steps/src/App.jsx
+++ app_steps/src/App.jsx
@@ -47,14 +47,16 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
-          <Button
-            bgColor="#e7e7e7"
-            textColor="#333"
-            onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-          >
-            Learn how
-          </Button>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <Button
+              bgColor="#e7e7e7"
+              textColor="#333"
+              onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+            >
+              Learn how
+            </Button>
+          </StepMessage>
 
           <div className="buttons">
             <Button
