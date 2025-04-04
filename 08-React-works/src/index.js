0e8c99a433fb3fd6a135c1819d9d2c272f75ca99
Huy Tran <baohuy82.th@gmail.com>
2025-04-03 23:18:14 -0700

    Update index.js

--- 08-React-works/src/index.js
+++ 08-React-works/src/index.js
@@ -7,17 +7,17 @@ import App from "./App";
 import "./index.css";
 
 // REACT 18
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// REACT 17
-// ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//   </React.StrictMode>
 // );
+
+// REACT 17
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
